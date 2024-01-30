e7b2733478987830221ba115696c48449c0926d9
Faizan Ahmed <153298469+Faizan-fzn@users.noreply.github.com>
2024-01-31 00:00:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,24 @@
 function getFormvalue() {
-    //Write your code here
+	
+      // Accessing the form element by its ID
+    var form = document.getElementById("form1");
+	
+     // Accessing the input elements by their names
+	var firstNameInput = form.elemen[fname];
+	var lastNameInput = form.elemen[lname];
+   
+	 // Getting the values entered by the user
+	var firstName = firstNameInput.value;
+	var LastName = LastNameInput.value;
 
+    // Checking if both first name and last name are provided
+	if(firstName && LastName){
+    // Displaying the first and last name using alert	
+	alert("First Name: " + firstName + "lastName: " + lastName);
+	} else {
+     // Displaying an alert if either first name or last name is missing
+	alert("Please enter both First Name and Last Name.");
+	}
+	 // Preventing the form from submitting and refreshing the page
+    return false;
 }
